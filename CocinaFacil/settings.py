c6229a4af27fc264287b4e60992f68a60a920ea6
wawayo <99446837+wawayo@users.noreply.github.com>
2024-06-30 23:04:29 -0400

    Update settings.py

--- CocinaFacil/settings.py
+++ CocinaFacil/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '0uoqpu$bn)56z^cv+y^wu3h%+)w%dcqkhx097
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'proyecto-djangopaee.onrender.com').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'https://proyectodjango-nqnh.onrender.com').split(',')
 
 # Application definition
 INSTALLED_APPS = [
